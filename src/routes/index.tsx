b42e6393b76a54eb29e0a6eebc532d57573fe3e8
JoleneKearse <pharaohnutz@gmail.com>
2024-04-07 17:50:10 -0400

    chore: add signin/out in header

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import header from "~/components/starter/header/header";
+
 export default component$(() => {
   return <></>;
 });
